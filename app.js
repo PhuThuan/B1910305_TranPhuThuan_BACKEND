4f774093db5de5e8fa25035e631cee836f4c9c25
PhuThuan <phuthuan1910@gmail.com>
2023-02-18 10:05:06 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -10,19 +10,19 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.json({ message: 'Welcome to contact book application!' })
 });
-app.use((req, res, next) => {
++app.use((req, res, next) => {
     // Code ở đây sẽ chạy khi không có route được định nghĩa nào
     // khớp với yêu cầu. Gọi next() để chuyển sang middleware xử lý lỗi
     return next(new ApiError(404, "Resource not found"));
-});
-// define error-handling middleware last, after other app.use() and routes calls
-app.use((err, req, res, next) => {
+  });
+  // define error-handling middleware last, after other app.use() and routes calls
+  app.use((err, req, res, next) => {
     // Middleware xử lý lỗi tập trung.
     // Trong các đoạn code xử lý ở các route, gọi next(error)
     // sẽ chuyển về middleware xử lý lỗi này
-    return res.status(error.statusCode || 500).json({
-        message: error.message || "Internal Server Error",
+    return res.status(err.statusCode || 500).json({
+      message: err.message || "Internal Server Error",
     });
-});
+  });
 
 module.exports = app;
